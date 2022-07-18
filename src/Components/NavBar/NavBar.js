e030b743d76e41f3895ed5205fd31ac0229c0dc2
MD. Sulaiman Hosain <solaiman.sh6@gmail.com>
2022-07-18 18:24:54 +0600

    Support button updated

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -73,52 +73,7 @@ export default function Example() {
 								<div className='absolute  inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
 									{/* Profile dropdown */}
 									<Menu as='div' className='ml-3 relative'>
-										<div>
-											<Menu.Button className='flex items-center'>
-												<span className='sr-only'>Open user menu</span>
-												<p>Support</p> &nbsp;
-												<img
-													className='w-3 mt-1'
-													src={DropDownIcon}
-													alt='dropdownIcon'
-												/>
-											</Menu.Button>
-										</div>
-										<Transition
-											as={Fragment}
-											enter='transition ease-out duration-100'
-											enterFrom='transform opacity-0 scale-95'
-											enterTo='transform opacity-100 scale-100'
-											leave='transition ease-in duration-75'
-											leaveFrom='transform opacity-100 scale-100'
-											leaveTo='transform opacity-0 scale-95'>
-											<Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
-												<Menu.Item>
-													{({ active }) => (
-														<Link
-															to={"terms_of_service"}
-															className={classNames(
-																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm text-gray-700'
-															)}>
-															Terms of Service
-														</Link>
-													)}
-												</Menu.Item>
-												<Menu.Item>
-													{({ active }) => (
-														<Link
-															to={"cookie_policy"}
-															className={classNames(
-																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm text-gray-700'
-															)}>
-															Cookie Policy
-														</Link>
-													)}
-												</Menu.Item>
-											</Menu.Items>
-										</Transition>
+										<Link to='/career'>Support</Link>
 									</Menu>
 								</div>
 							</div>
